347c5a0a9525500a8d73d1c9c6e4bc88211466af
Kyle Peatt <kpeatt@gmail.com>
2014-07-30 09:02:37 -0700

    Add new version of shim and velocity

--- examples/assets/js/config.js
+++ examples/assets/js/config.js
@@ -4,8 +4,8 @@ require.config({
         'text': 'bower_components/requirejs-text/text',
         'selectorEngine': 'lib/zeptojs/dist/zepto.min',
         'zappy': 'bower_components/tappy/tappy',
-        'velocity-shim': 'lib/velocity-shim',
-        'velocity': 'bower_components/velocity/jquery.velocity',
+        'velocity-shim': 'lib/velocity.jquery-shim',
+        'velocity': 'lib/jquery.velocity',
         'bellows': 'dist/bellows'
     },
     'shim': {
